5b7a0f26b433ac63bed05350de896bdc17820621
lrh <lirh@gmail.com>
2021-01-07 09:12:46 +0800

    aop features

---
+++ spring-aop/aop-features/src/main/java/com/lrh/aop/features/AspectjXmlDemo.java
@@ -0,0 +1,29 @@
+package com.lrh.aop.features;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author lirh
+ * @version 2021年01月07日 8:28 上午
+ */
+
+public class AspectjXmlDemo {
+
+  public static void main(String[] args) {
+    ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/spring-aop-conetext.xml");
+
+
+    applicationContext.refresh();
+
+    AspectjXmlDemo aspectjAnnotationDemo = applicationContext.getBean(AspectjXmlDemo.class);
+    aspectjAnnotationDemo.execute();
+
+    applicationContext.close();
+  }
+
+  public void execute() {
+    System.out.println("execute()。。。。。。");
+  }
+
+
+}
